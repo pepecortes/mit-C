57572e11de1f4386494f960f2edc1f5fdc695943
pepecortes <jcortesocana@gmail.com>
2013-09-07 16:45:08 +0200

    continue assignment 5

--- assignment5/btree.c
+++ assignment5/btree.c
@@ -18,9 +18,49 @@ struct tnode* talloc(int data)
 	return output; 
 	}
 	
+struct tnode* addnode(struct tnode* root, int data)
+	/*
+	add the data to the root node.
+	recursive. data goes to the right of left. if position
+	is not free, goes down the tree
+	*/
+	{
+	struct tnode* new = NULL;
+	if (root == NULL) // the positions is free: go ahead
+		{
+		new = (struct tnode*)malloc(sizeof(struct tnode));
+		new->data = data;
+		}
+	else if (data < root->data)
+		root->left = addnode(root->left, data);
+	else
+		root->right = addnode(root->right, data);
+	return new;
+	}
+	
+void preorder(struct tnode* root)
+	/*to display the elements using pre-order traver­sal*/
+	{
+	if ((root->left == NULL) && (root->right == NULL))
+		printf("# %i #\n", root->data);
+	if (root->left != NULL) //print left first
+		preorder(root->left);
+	if (root->right != NULL)
+		preorder(root->right);
+	}
+
+	
 
 int main(void)
 	{
 	struct tnode *pn = talloc(11);
+	int input[] = {3, 1, 0, 2, 8, 6, 5, 9}, input_size, i;
+	input_size = sizeof(input)/sizeof(int);
+	for  (i = 0; i<input_size; i++)
+		{
+		printf("adding %i\n", input[i]);
+		addnode(pn, input[i]);
+		}
+	preorder(pn);
 	return 0;
 	}
